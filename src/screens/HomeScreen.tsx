0a36152134cd74da9178c35086b7ba6755e8034e
MaryChriss <marychris.crf@gmail.com>
2025-05-28 15:20:32 -0300

    feature:  fix home and profile

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,219 +1,236 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
 import Header from "../../components/header/header";
 import { TextInput } from "react-native-paper";
-import MapView, { Marker } from 'react-native-maps';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import MapView, { Marker } from "react-native-maps";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Home() {
-    const [text, setText] = useState('');
-    const mapvision = [
-  {
-    "featureType": "administrative",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  }
-]
+  const [text, setText] = useState("");
+  const mapvision = [
+    {
+      featureType: "administrative",
+      elementType: "geometry",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "poi",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.icon",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "road.arterial",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "road.local",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "transit",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+  ];
 
-    return (
-        <View style={styles.container}>
-            <Header />
+  return (
+    <View style={styles.container}>
+      <Header />
 
-            <View style={styles.search}>
-                <TextInput
-                style={styles.input}
-                value={text}
-                onChangeText={setText}
-                underlineColor="transparent"
-                mode="outlined"
-                outlineColor="#fff"
-                activeOutlineColor="#fff"
-                theme={{ colors: { text: '#000', background: '#fff' } }}
-                placeholder="Pesquisar local"
-                />
+      <View style={styles.search}>
+        <TextInput
+          style={styles.input}
+          value={text}
+          onChangeText={setText}
+          underlineColor="transparent"
+          mode="outlined"
+          outlineColor="#fff"
+          activeOutlineColor="#fff"
+          theme={{ colors: { text: "#000", background: "#fff" } }}
+          placeholder="Pesquisar local"
+        />
+      </View>
+
+      <ScrollView>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: -8.0476,
+            longitude: -34.877,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+          customMapStyle={mapvision}
+        >
+          <Marker coordinate={{ latitude: -8.0476, longitude: -34.877 }} />
+        </MapView>
+
+        <View style={styles.card}>
+          <View style={styles.header}>
+            <MaterialCommunityIcons
+              name="weather-rainy"
+              size={48}
+              color="#000"
+              style={styles.icon}
+            />
+            <View>
+              <Text style={styles.city}>Recife</Text>
+              <Text style={styles.condition}>Chuva moderada</Text>
             </View>
+          </View>
 
-            <MapView
-                style={styles.map}
-                initialRegion={{
-                    latitude: -8.0476,
-                    longitude: -34.8770,
-                    latitudeDelta: 0.05,
-                    longitudeDelta: 0.05,
-                }}
-                customMapStyle={mapvision}
-                >
-                <Marker coordinate={{ latitude: -8.0476, longitude: -34.8770 }} />
-            </MapView>
-
-            <View style={styles.card}>
-                <View style={styles.header}>
-                    <MaterialCommunityIcons name="weather-rainy" size={48} color="#000" style={styles.icon} />
-                    <View>
-                    <Text style={styles.city}>Recife</Text>
-                    <Text style={styles.condition}>Chuva moderada</Text>
-                    </View>
-                </View>
-
-                <Text style={styles.temp}>26 °C</Text>
-                <Text style={styles.info}>Umidade: 757</Text>
-                <Text style={styles.info}>Vento: w8r4/h</Text>
-            </View>
-
+          <Text style={styles.temp}>26 °C</Text>
+          <Text style={styles.info}>Umidade: 757</Text>
+          <Text style={styles.info}>Vento: w8r4/h</Text>
         </View>
-    );
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-filialTitle: {
-    color: '#fff',
+  filialTitle: {
+    color: "#fff",
     fontSize: 15,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 150,
     marginLeft: 40,
-},
-container: {
+  },
+  container: {
     flex: 1,
-},
-card: {
-    backgroundColor: '#fff',
+  },
+  card: {
+    backgroundColor: "#fff",
     padding: 16,
     borderRadius: 16,
-    width: '90%',
-    alignSelf: 'center',
+    width: "90%",
+    alignSelf: "center",
     marginTop: 40,
     elevation: 3,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 4,
     shadowOffset: { width: 0, height: 2 },
-},
-header: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    marginBottom: 30,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 12,
-},
-icon: {
+  },
+  icon: {
     marginRight: 12,
-},
-city: {
+  },
+  city: {
     fontSize: 18,
-    fontWeight: '600',
-},
-condition: {
+    fontWeight: "600",
+  },
+  condition: {
     fontSize: 14,
-    color: '#555',
-},
-temp: {
+    color: "#555",
+  },
+  temp: {
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 6,
-},
-info: {
+  },
+  info: {
     fontSize: 14,
-    color: '#333',
-},
-map: {
-  width: '90%',
-  height: 280, 
-  alignSelf: 'center',
-  marginTop: 46,
-  overflow: 'hidden', 
-},
-search: {
-    backgroundColor: '#ABD5FF',
-    width: '100%',
+    color: "#333",
+  },
+  map: {
+    width: "90%",
+    height: 280,
+    alignSelf: "center",
+    marginTop: 26,
+    overflow: "hidden",
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  search: {
+    backgroundColor: "#ABD5FF",
+    width: "100%",
     height: 80,
-    marginTop:0,
-},
-input: {
-    backgroundColor: '#fff',
+    marginTop: 0,
+  },
+  input: {
+    backgroundColor: "#fff",
     height: 40,
     fontSize: 16,
-    width: '90%',
+    width: "90%",
     marginLeft: 20,
     marginTop: 15,
-},
+  },
 });
--- src/screens/Perfil.tsx
+++ src/screens/Perfil.tsx
@@ -1,20 +1,31 @@
-import { Image, Text, View, StyleSheet, TouchableOpacity, FlatList } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Pressable,
+} from "react-native";
 import Header from "../../components/header/header";
 import { useCallback, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { FontAwesome } from "@expo/vector-icons";
+import { TextInput } from "react-native-paper";
 
 export default function Perfil() {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [favorites, setFavorites] = useState<any[]>([]);
-
+  const [modalVisible, setModalVisible] = useState(false);
   const navigation = useNavigation<any>();
   const favoritos = "favorites";
 
   useEffect(() => {
     const loadSavedData = async () => {
-      const data = await AsyncStorage.getItem('userData');
+      const data = await AsyncStorage.getItem("userData");
       if (data) {
         const user = JSON.parse(data);
         setEmail(user.email);
@@ -25,71 +36,138 @@ export default function Perfil() {
   }, []);
 
   useFocusEffect(
-  useCallback(() => {
-    const loadFavorites = async () => {
-      const favs = await AsyncStorage.getItem(favoritos);
-      setFavorites(favs ? JSON.parse(favs) : []);
-    };
+    useCallback(() => {
+      const loadFavorites = async () => {
+        const favs = await AsyncStorage.getItem(favoritos);
+        setFavorites(favs ? JSON.parse(favs) : []);
+      };
 
-    loadFavorites();
-  }, [])
-);
+      loadFavorites();
+    }, [])
+  );
 
-const removeFavorite = async (cidade: string) => {
-  const updated = favorites.filter((c) => c !== cidade);
-  setFavorites(updated);
-  await AsyncStorage.setItem(favoritos, JSON.stringify(updated));
-};
+  const removeFavorite = async (cidade: string) => {
+    const updated = favorites.filter((c) => c !== cidade);
+    setFavorites(updated);
+    await AsyncStorage.setItem(favoritos, JSON.stringify(updated));
+  };
 
-const renderItem = ({ item }: { item: string }) => (
-  <View style={styles.card}>
-    <View style={styles.info}>
-      <Text style={styles.name}>{item}</Text>
+  const renderItem = ({ item }: { item: string }) => (
+    <View style={styles.card}>
+      <View style={styles.info}>
+        <View>
+          <Text style={styles.name}>{item}</Text>
+        </View>
 
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.buttonHome} onPress={() => navigation.navigate('Home')}>
-          <Text style={styles.buttonHomeText}>Ver</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+            style={styles.buttonHome}
+            onPress={() => navigation.navigate("Home")}
+          >
+            <Text style={styles.buttonHomeText}>Ver</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.buttonRemove} onPress={() => removeFavorite(item)}>
-          <Text style={styles.buttonRemoveText}>X</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => removeFavorite(item)}>
+            <FontAwesome name="trash" size={20} color={"#e53935"} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
-  </View>
-);
-
+  );
 
   return (
     <View style={styles.container}>
       <Header />
-      
+
       <View style={styles.profileContainer}>
-        <Image style={styles.image} source={require('../../assets/photo.png')} />
+        <Image
+          style={styles.image}
+          source={require("../../assets/photo.png")}
+        />
 
         <View style={styles.infosContainer}>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.email}>{email}</Text>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.email}>{email}</Text>
 
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Editar Perfil</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setModalVisible(true)}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Editar Perfil</Text>
+          </TouchableOpacity>
         </View>
       </View>
 
-        <View style={{ display: "flex",padding: 10 }}>
-            <Text style={styles.title}>Cidades favoritas:</Text>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Editar perfil</Text>
+            <Text style={styles.modalSubtitle}>
+              Altere seu nome ou e-mail quando quiser.
+            </Text>
 
-            {favorites.length === 0 ? (
-            <Text style={styles.emptyText}>Nenhum personagem salvo.</Text>
-            ) : (
-            <FlatList
-                data={favorites}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={renderItem}
-                />
-            )}
+            <TextInput
+              placeholder="Nome"
+              mode="outlined"
+              value={name}
+              onChangeText={setName}
+              style={styles.input}
+              theme={{
+                roundness: 12,
+                colors: {
+                  primary: "#FF8F53",
+                  background: "#fff",
+                  placeholder: "#aaa",
+                  text: "#000",
+                },
+              }}
+            />
+
+            <TextInput
+              placeholder="E-mail"
+              mode="outlined"
+              value={email}
+              onChangeText={setEmail}
+              style={styles.input}
+              theme={{
+                roundness: 12,
+                colors: {
+                  primary: "#FF8F53",
+                  background: "#fff",
+                  placeholder: "#aaa",
+                  text: "#000",
+                },
+              }}
+            />
+
+            <Pressable
+              style={styles.button}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.buttonText}>Fechar</Text>
+            </Pressable>
+          </View>
         </View>
+      </Modal>
+
+      <View style={{ display: "flex", padding: 10 }}>
+        <Text style={styles.title}>Cidades favoritas:</Text>
+
+        {favorites.length === 0 ? (
+          <Text style={styles.emptyText}>Nenhuma cidade salva.</Text>
+        ) : (
+          <FlatList
+            data={favorites}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={renderItem}
+          />
+        )}
+      </View>
     </View>
   );
 }
@@ -97,110 +175,129 @@ const renderItem = ({ item }: { item: string }) => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: "column",
   },
-    title: {
-    fontSize: 24,
+  title: {
+    fontSize: 18,
     fontWeight: "bold",
     marginBottom: 20,
-    textAlign: "center",
-    marginTop: 30,
+    textAlign: "left",
+    marginTop: 40,
   },
-    emptyText: {
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 25,
+    width: "80%",
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 10,
+    textAlign: "left",
+    color: "#333",
+  },
+  modalSubtitle: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 16,
+    textAlign: "left",
+    color: "#333",
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 8,
+    backgroundColor: "#FF8F53",
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  emptyText: {
     textAlign: "center",
     fontSize: 18,
     color: "#888",
-    marginTop: 360,
+    marginTop: 150,
   },
   buttonsContainer: {
-  flexDirection: "row",
-  gap: 5,
-  alignItems: "center",
-  marginLeft: 15,
-},
-
-    card: {
     flexDirection: "row",
-    marginBottom: 15,
-    backgroundColor: "#ffffff",
-    borderRadius: 8,
-    overflow: "hidden",
-    shadowColor: "#000",
+    gap: 5,
+    alignItems: "center",
+    justifyContent: "flex-end",
   },
-buttonHome: {
-  borderWidth: 1,
-  borderColor: "#00000045",
-  borderRadius: 5,
-  width: 60,
-  height: 30,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#f2f2f2',
-},
 
-
-buttonRemove: {
-  backgroundColor: "#cf1212c7",
-  borderRadius: 5,
-  width: 60,
-  height: 30,
-  justifyContent: 'center',
-  alignItems: 'center',
-},
-
-buttonHomeText: {
-  textAlign: 'center',
-  color: '#000',
-  fontWeight: '600',
-},
-buttonRemoveText: {
-  color: "white",
-  textAlign: 'center',
-  fontWeight: 'bold',
-},
-    info: {
+  card: {
+    backgroundColor: "#f8f8f8",
     padding: 10,
-    flexDirection: 'row'
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: "#0000001a",
+    borderWidth: 1,
+  },
+  buttonHome: {
+    borderWidth: 1,
+    borderColor: "#00000045",
+    borderRadius: 5,
+    width: 60,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+    marginRight: 5,
+  },
+
+  buttonHomeText: {
+    textAlign: "center",
+    color: "#000",
+    fontWeight: "600",
+  },
+  buttonRemoveText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  info: {
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
   profileContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     gap: 30,
-    marginTop: 50
+    marginTop: 50,
   },
   infosContainer: {
-        display: 'flex',
+    display: "flex",
   },
   image: {
-    width: 150,
-    height: 150,
+    width: 120,
+    height: 120,
   },
   name: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 17,
+    fontWeight: "bold",
     marginBottom: 5,
   },
   email: {
-    fontSize: 16,
-    color: 'gray',
+    fontSize: 15,
+    color: "gray",
   },
-    button: {
-  marginTop: 10,
-  backgroundColor: '#FF8F53',
-  borderRadius: 10,
-  width: 120,
-  height: 35,
-  paddingVertical: 10,
-  alignItems: 'center',
-  justifyContent: 'center',
-    alignSelf: 'center',
-},
-buttonText: {
-  color: '#ffffff',
-  fontSize: 14,
-  fontWeight: 'bold',
-  textAlign: 'center',
-}
 });
